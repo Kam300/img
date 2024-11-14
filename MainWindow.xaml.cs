d00fe4a0a77e14843bee1a35d7379432a83942e5
Kam300 <kamilgaripov205@gmail.com>
2024-11-15 00:04:25 +0500

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ImageViewer
+{
+    public partial class MainWindow : Window
+    {
+        private double currentAngle = 0; // хранение текущего угла поворота
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RotateLeft_Click(object sender, RoutedEventArgs e)
+        {
+            currentAngle -= 90; // Уменьшаем угол на 90 градусов
+            RotateImage(image1);
+        }
+
+        private void RotateRight_Click(object sender, RoutedEventArgs e)
+        {
+            currentAngle += 90; // Увеличиваем угол на 90 градусов
+            RotateImage(image1);
+        }
+
+        private void RotateImage(Image image)
+        {
+            // Создаем новый RotateTransform и применяем его к изображению
+            RotateTransform rotateTransform = new RotateTransform(currentAngle);
+            image.RenderTransform = rotateTransform;
+        }
+    }
+}
